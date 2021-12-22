49778763d70a7db25f2213a8f035cf8f1426cb74
João Vitor <jsoareez@gmail.com>
2021-12-21 22:16:36 -0300

    json and routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
+import { routes } from "./routes";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({
-    message: "Servidor rodando",
-  });
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () =>
   console.log("🔥 Server started at http://localhost:3000")
